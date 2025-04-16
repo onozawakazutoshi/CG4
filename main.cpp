993d6dba9d29e4b8f7fe0a6d1e5343ed5b961b74
onozawakazutoshi <146906422+onozawakazutoshi@users.noreply.github.com>
2025-04-16 15:26:18 +0900

    //

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <Windows.h>
+#include <KamataEngine.h>
+
+using namespace KamataEngine;
+
+DirectXCommon* dxcommon = DirectXCommon::GetInstance();
 
 // Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) { 
+
+	KamataEngine::Initialize();
 	
+	while (true) {
+		if (KamataEngine::Update()) {
+			
+			break;
+		}
+		dxcommon->PreDraw();
+		dxcommon->PostDraw();
+	}
+
+	
+
+	KamataEngine::Finalize();
 	return 0;
 }
