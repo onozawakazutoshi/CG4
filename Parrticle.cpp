6f29e38966b36be10d15a42f4050f2c23af3492d
onozawakazutoshi <146906422+onozawakazutoshi@users.noreply.github.com>
2025-04-17 12:02:24 +0900

    //完成

--- Parrticle.cpp
+++ Parrticle.cpp
@@ -1,17 +1,27 @@
 #include "Parrticle.h"
 
+using namespace MathUtility;
+
 void Parrticle::Initialize(Model* model) { 
 	assert(model);
 	model_ = model;
 	worldtransform_.Initialize();
+
+	objectColor_.Initialize();
+	color_ = {1, 1, 0, 1};
+
 }
 
 void Parrticle::Update() { 
-	worldtransform_.TransferMatrix();
+	worldtransform_.translation_ += {0.0f, 0.1f, 0.0f};
+	//worldtransform_.TransferMatrix();
 
+	worldtransform_.UpdateMatrix();
+
+	objectColor_.SetColor(color_);
 }
 
 void Parrticle::Draw(Camera& camera) { 
-	model_->Draw(worldtransform_, camera);
+	model_->Draw(worldtransform_, camera,&objectColor_);
 
 }
--- Parrticle.h
+++ Parrticle.h
@@ -14,5 +14,7 @@ public:
 private:
 	WorldTransform worldtransform_;
 	Model* model_ = nullptr;
+	ObjectColor objectColor_;
+	Vector4 color_;
 
 };
